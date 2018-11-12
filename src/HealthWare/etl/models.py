958e807e750fca9ba1accd2e7f97f7556078ec52
Felipe de Jesus Ruiz Garcia <felipe_57@live.com.mx>
2018-11-11 18:45:24 -0600

    Add Model Query
    Query model come into the SQL querys to retrieve the data
    store by the different databases.
    
    Signed-off-by: Felipe de Jesus Ruiz Garcia <felipe_57@live.com.mx>

--- src/HealthWare/etl/models.py
+++ src/HealthWare/etl/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+# universal select for different databases
+
+class Query(models.Model):
+    database = models.CharField(max_length=200)
+    select = models.CharField(max_length=200)
+    having = models.CharField(max_length=200)
+    group = models.CharField(max_length=200)
+    sort = models.CharField(max_length=200)
+    where = models.CharField(max_length=200)
+    nestedSelect = models.CharField(max_length=200)
