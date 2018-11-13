58f2e040e8ec56749ccbf4b2c7320e36b2d443ea
Felipe de Jesus Ruiz Garcia <felipe_57@live.com.mx>
2018-11-12 20:16:22 -0600

    Add login view
    Add view login etl/views.py and etl/urls.py
    
    Signed-off-by: Felipe de Jesus Ruiz Garcia <felipe_57@live.com.mx>

--- src/HealthWare/etl/views.py
+++ src/HealthWare/etl/views.py
@@ -1,3 +1,41 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.views import generic
 
-# Create your views here.
+from .forms import Log
+
+BASE = {
+'BASE_TITLE' : "ETL",
+'BASE_MAIN_TITLE' : "Extract Load Transformation Data System",
+'BASE_MODAL_TITLE' : "etl",
+'BASE_MODAL_CLOSE' : "Login",
+'BASE_FOOTER_TITLE' : "MENU",
+'BASE_FOOTER_SUBTITLE' : "ETL SYSTEM",
+'BASE_FOOTER_MEDIUM' : "REGRESAR ARRIBA",
+'BASE_FOOTER_LAST' : "Diana Hernandez, Felipe Ruiz",
+'form' : ''
+}
+
+def login(request):
+	if request.method == 'POST':
+		form = Log(request.POST)
+		if form.is_valid():
+
+			if form.cleaned_data['user'] == 'admin' \
+			and form.cleaned_data['password'] == '123':
+
+				return HttpResponseRedirect('handle')
+
+			else:
+				BASE['form'] = Log()
+				BASE['BASE_FOOTER_TITLE'] = "wrong password"
+				return render(request, 'etl/login.html', BASE )
+		else:
+			return HttpResponseRedirect('/wrong/')
+
+	# if a GET (or any other method) we'll create a blank form
+	else:
+		BASE['form'] = Log()
+		return render(request, 'etl/login.html', BASE )
+
